abf6b6352f1e9494ff99ebc12cb7460384cd9dc3
schlez <rotemroy12@gmail.com>
2015-11-03 16:55:43 +0200

    Timing

---
+++ src/org/usfirst/frc/team2212/robot/subsystems/Timing.java
@@ -0,0 +1,27 @@
+package org.usfirst.frc.team2212.robot.subsystems;
+
+import edu.wpi.first.wpilibj.TalonSRX;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class Timing extends Subsystem {
+    private TalonSRX t1,t2;
+    public Timing(TalonSRX t1,TalonSRX t2){
+    	this.t1=t1;
+    	this.t2=t2;
+    }
+    public void charge(double speed){
+    	t1.set(speed);
+    	t2.set(speed);
+    }
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
+}
+
